927b3bcfe7512072ecc82f6085754a4df25c880c
Farnix <you@example.com>
2019-10-28 12:08:37 +0700

    Create proj

---
+++ JSONTest/JSONTest/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JSONTest.Models.Home;
+
+namespace JSONTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetUsers()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { Age = 15, Name = "Dima" });
+            users.Add(new User() { Age = 19, Name = "Matvey" });
+            users.Add(new User() { Age = 12, Name = "Anton" });
+
+            var usersJSON = users;
+
+            return Json(usersJSON, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
